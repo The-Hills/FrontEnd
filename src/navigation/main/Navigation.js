0226004a01a25c8040a7e9e5e85ecae96ebf68c6
hqtriaidenboizz <hoquoctri10b7@gmail.com>
2023-02-07 17:19:01 +0700

    Test Auth

--- src/navigation/main/Navigation.js
+++ src/navigation/main/Navigation.js
@@ -1,32 +1,65 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import DriverHomeScreen from '../../screens/home/DriverHomeScreen';
 import SplashSceen from '../../screens/Intro/SplashSceen';
 import Onboarnding from '../../screens/Intro/Onboarnding';
-import UserHomeScreen from '../../screens/home/UserHomeScreen';
 import BottomTabs from '../home/BottomTabs';
 import Register from '../../screens/auth/register/Register';
 import Login from '../../screens/auth/login/Login';
+import UserHomeScreen from '../../screens/home/UserHomeScreen';
 
 const Stack = createNativeStackNavigator();
 const Navigation = () => {
+  const [initialRouteName, setInitialRouteName] = React.useState('');
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      authUser();
+    }, 2000);
+  }, []);
+
+  const authUser = async () => {
+    try {
+      let userData = await AsyncStorage.getItem('userData');
+      if (userData) {
+        userData = JSON.parse(userData);
+        if (userData.loggedIn) {
+          setInitialRouteName('BottomTabs');
+        } else {
+          setInitialRouteName('Login');
+        }
+      } else {
+        setInitialRouteName('SplashSceen');
+      }
+    } catch (error) {
+      setInitialRouteName('SplashSceen');
+    }
+  };
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="SplashSceen"
-        screenOptions={{headerShown: false}}
-        >
-        <Stack.Screen name="BottomTabs" component={BottomTabs} />
-        <Stack.Screen name="DriverHomeScreen" component={DriverHomeScreen} />
-        <Stack.Screen name="Onboarnding" component={Onboarnding} />
-        <Stack.Screen name="SplashSceen" component={SplashSceen} />
-        <Stack.Screen name="Register" component={Register} />
-        <Stack.Screen name="Login" component={Login} />
-
-
-      </Stack.Navigator>
+      {!initialRouteName ? (
+        <></>
+      ) : (
+        <>
+          <Stack.Navigator
+            initialRouteName={initialRouteName}
+            screenOptions={{headerShown: false}}>
+            <Stack.Screen name="SplashSceen" component={SplashSceen} />
+            <Stack.Screen name="BottomTabs" component={BottomTabs} />
+            <Stack.Screen
+              name="DriverHomeScreen"
+              component={DriverHomeScreen}
+            />
+            <Stack.Screen name="Onboarnding" component={Onboarnding} />
+            <Stack.Screen name="Register" component={Register} />
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="UserHomeScreen" component={UserHomeScreen} />
+          </Stack.Navigator>
+        </>
+      )}
     </NavigationContainer>
   );
 };
--- src/screens/home/UserHomeScreen.jsx
+++ src/screens/home/UserHomeScreen.jsx
@@ -1,12 +1,36 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import {GeneralStyle} from '../../styles/generalStyles';
 import {Colors} from '../../../assets/theme/colors';
+import Button from '../../components/general/Button';
 
-const UserHomeScreen = () => {
+const UserHomeScreen = ({navigation}) => {
+  const [userDetails, setUserDetails] = React.useState();
+  React.useEffect(() => {
+    getUserData();
+  }, []);
+
+  const getUserData = async () => {
+    const userData = await AsyncStorage.getItem('userData');
+    if (userData) {
+      setUserDetails(JSON.parse(userData));
+    }
+  };
+
+  const logout = () => {
+    AsyncStorage.setItem(
+      'userData',
+      JSON.stringify({...userDetails, loggedIn: false}),
+    );
+    navigation.navigate('Login');
+  };
   return (
     <View style={GeneralStyle.container}>
-      <Text style={[GeneralStyle.text, styles.text]}>UserHomeScreen</Text>
+      <Text style={[GeneralStyle.text, styles.text]}>
+        Hello {userDetails?.fullname}
+      </Text>
+      <Button lable="Logout" onPress={logout} />
     </View>
   );
 };
